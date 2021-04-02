7f4a2f257712e300645fda253eb9dd2f6d609ef0
Alejandro Torres <atorresalcala.me@gmail.com>
2021-04-02 13:58:14 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import DefaultTheme from './Utils/Theme';
+import Dashboard from './Components/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={createMuiTheme(DefaultTheme)}>
+			<Router>
+				<Switch>
+					<Route path='/'>
+						<Dashboard />
+					</Route>
+				</Switch>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { useHistory } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+	wrapperDashboard: {
+		width: '100%',
+		height: 'auto',
+		minHeight: '100vh',
+		backgroundColor: 'red',
+	},
+});
+
+function Dashboard(props) {
+	let history = useHistory();
+	const { classes } = props;
+
+	const handlePanelState = value => {
+		const url = String(value);
+		localStorage.setItem('current', url);
+		history.push(`/${url}`);
+	};
+
+	return <Grid container className={classes.wrapperDashboard}></Grid>;
+}
+
+export default withStyles(styles)(Dashboard);
